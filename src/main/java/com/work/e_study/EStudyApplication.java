717619749e5b959daf91a9963731babe59ce0895
vladislavsIvanovs13 <vladislavs.ivanovs_3@edu.rtu.lv>
2024-11-08 01:36:58 +0200

    Add Spring Boot Initializer

---
+++ src/main/java/com/work/e_study/EStudyApplication.java
@@ -0,0 +1,13 @@
+package com.work.e_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStudyApplication.class, args);
+	}
+
+}
