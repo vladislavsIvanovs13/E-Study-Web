cafd825e02d8e618a519b4ebaf5edb1ef7c1ffb8
vladislavsIvanovs13 <vladislavs.ivanovs_3@edu.rtu.lv>
2024-11-25 12:33:05 +0200

    Connect FE to BE

--- frontend/src/pages/Professors.jsx
+++ frontend/src/pages/Professors.jsx
@@ -1,24 +1,24 @@
 import { NavBar, Profile, ModalAdd, ModalDelete, ModalEdit } from '../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
+import axios from "axios";
 
 export default function Campus() {
 
   const [showAdd, setShowAdd] = useState(false);
   const [showDelete, setShowDelete] = useState(false);
   const [showEdit, setShowEdit] = useState(false);
+  const [employeeCourses, setEmployeeCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const data = [
-    { id: 1, title: 'Course', employee: 'aaa',
-      structuralUnit: 'DITF', room: 'Zunda',
-      email: 'longtext@gmail.com' },
-    { id: 2, title: 'Course', employee: 'bbb',
-      structuralUnit: 'DITF', room: 'Secret office 1234567890',
-      email: 'longtext@gmail.com' },
-    { id: 4, title: 'Course', employee: 'bbb',
-      structuralUnit: 'DITF', room: 'Secret office 1234567890',
-      email: 'longtext@gmail.com' }
-  ];
+  useEffect(() => {
+    // Make GET request to Spring Boot backend
+    axios.get('http://localhost:8080/employees')
+        .then(response => {
+          setEmployeeCourses(response.data);
+          setLoading(false)
+        })
+  }, []);
 
     return (
         <div className="mx-6 mt-12 text-sm sm:mx-24 sm:text-base relative">
@@ -26,7 +26,7 @@ export default function Campus() {
           <NavBar/>
           <div className="mt-12">
             <table border="2" style={{width: '100%'}}>
-              <thead className="h-[50px] border-2 text-center border-rtu-green bg-[#679999] text-rtu-green mt-12 relative">
+              <thead className="fade-in-row h-[50px] border-2 text-center border-rtu-green bg-[#679999] text-rtu-green mt-12 relative">
                 <tr>
                   <th className="border-r-2 border-rtu-green">Kursa nosaukums</th>
                   <th className="border-r-2 border-rtu-green">Mācībspēks</th>
@@ -37,8 +37,8 @@ export default function Campus() {
                 </tr>
               </thead>
               <tbody>
-                {data.map(row => (
-                    <tr className="h-[50px]" key={row.id}>
+                {employeeCourses.map(row => (
+                    <tr className="h-[50px] fade-in-row" key={`${row.employeeId}-${row.courseId}`}>
                       <td className="border-2 text-center border-rtu-green bg-[#9ebdbd] text-rtu-green mt-12">{row.title}</td>
                       <td className="border-2 text-center border-rtu-green bg-[#9ebdbd] text-rtu-green mt-12">{row.employee}</td>
                       <td className="border-2 text-center border-rtu-green bg-[#9ebdbd] text-rtu-green mt-12">{row.structuralUnit}</td>
@@ -67,27 +67,17 @@ export default function Campus() {
             </table>
           </div>
 
-            {/*<h1 className="border-r-2 border-b-2 border-rtu-green">Datoru organizācija un asambleri</h1>*/}
-            {/*<h2 className="border-r-2 border-b-2 border-rtu-green">Uldis Sukovskis</h2>*/}
-            {/*<h1 className="border-r-2 border-b-2 border-rtu-green">Lietišķo datorsistēmu institūts</h1>*/}
-            {/*<h1 className="border-r-2 border-b-2 border-rtu-green">Zunda krastmala 10-325</h1>*/}
-            {/*<h1 className="border-b-2 border-rtu-green">uldis.sukovskis@edu.rtu.lv</h1>*/}
-
-            {/*<h1 className="border-r-2 border-rtu-green">Sistēmu analīze un zināšanu iegūšana</h1>*/}
-            {/*<h2 className="border-r-2 border-rtu-green">Mārīte Kirikova</h2>*/}
-            {/*<h1 className="border-r-2 border-rtu-green">Lietišķo datorsistēmu institūts</h1>*/}
-            {/*<h1 className="border-r-2 border-rtu-green">Zunda krastmala 10-421</h1>*/}
-            {/*<h1 className="border-rtu-green">marite.kirikiva@edu.rtu.lv</h1>*/}
-
-          <div className="sm:flex mt-6">
-            <button onClick={() => setShowAdd(true)}
-                    className="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Pievienot
-            </button>
-            {showAdd && createPortal(
-                <ModalAdd onClose={() => setShowAdd(false)}/>,
-                document.body
-            )}
-          </div>
+        {!loading && (
+            <div className="sm:flex mt-6 mb-6">
+                <button onClick={() => setShowAdd(true)}
+                        className="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Pievienot
+                </button>
+                {showAdd && createPortal(
+                    <ModalAdd onClose={() => setShowAdd(false)}/>,
+                    document.body
+                )}
+            </div>
+        )}
         </div>
 
     );
