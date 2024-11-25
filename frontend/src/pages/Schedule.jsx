1657a538d09e5b8cfffbcc1cff60dd4fbebfb92d
vladislavsIvanovs13 <vladislavs.ivanovs_3@edu.rtu.lv>
2024-11-25 02:33:01 +0200

    Update FE Employee table

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -9,53 +9,52 @@ export default function Schedule() {
   const [showGrade, setShowGrade] = useState(false);
 
     return (
-      <div class="mx-6 mt-12 text-sm sm:mx-24 sm:text-base">
+      <div className="mx-6 mt-12 text-sm sm:mx-24 sm:text-base">
         <Profile />
         <NavBar />
-          <div class="grid grid-cols-8 border-2 text-center border-rtu-green bg-[#9ebdbd] text-rtu-green mt-12 min-w-[1100px]">
+          <div className="grid grid-cols-8 border-2 text-center border-rtu-green bg-[#9ebdbd] text-rtu-green mt-12 min-w-[1100px]">
+              <h1 className="col-span-8 border-b-2 border-rtu-green">Pirmdiena (28.10.2024.)</h1>
 
-              <h1 class="col-span-8 border-b-2 border-rtu-green">Pirmdiena (28.10.2024.)</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Laiks</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Studiju kurss</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Auditorija</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Mācībspēks</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Tēma</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Mājasdarbs</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Atzīme</h1>
+              <h1 className="border-b-2 border-rtu-green">Komentārs</h1>
 
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Laiks</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Studiju kurss</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Auditorija</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Mācībspēks</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Tēma</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Mājasdarbs</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Atzīme</h1>
-              <h1 class="border-b-2 border-rtu-green">Komentārs</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">10:15-11:50</h1>
+              <h2 className="border-r-2 border-b-2 border-rtu-green"><b>Lekc. </b>Datoru organizācija un asambleri</h2>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Āze. 12-102</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green">Uldis Sukovskis</h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green"></h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green"></h1>
+              <h1 className="border-r-2 border-b-2 border-rtu-green"></h1>
+              <h1 className="border-b-2 border-rtu-green"></h1>
 
-              <h1 class="border-r-2 border-b-2 border-rtu-green">10:15-11:50</h1>
-              <h2 class="border-r-2 border-b-2 border-rtu-green"><b>Lekc. </b>Datoru organizācija un asambleri</h2>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Āze. 12-102</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green">Uldis Sukovskis</h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green"></h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green"></h1>
-              <h1 class="border-r-2 border-b-2 border-rtu-green"></h1>
-              <h1 class="border-b-2 border-rtu-green"></h1>
-
-              <h1 class="border-r-2 border-rtu-green">12:30-14:05</h1>
-              <h2 class="border-r-2 border-rtu-green"><b>Lekc. </b>Sistēmu analīze un zināšanu iegūšana</h2>
-              <h1 class="border-r-2 border-rtu-green">Āze. 12-102</h1>
-              <h1 class="border-r-2 border-rtu-green">Mārīte Kirikova</h1>
-              <h1 class="border-r-2 border-rtu-green"></h1>
-              <h1 class="border-r-2 border-rtu-green"></h1>
-              <h1 class="border-r-2 border-rtu-green"></h1>
-              <h1 class=""></h1>
+              <h1 className="border-r-2 border-rtu-green">12:30-14:05</h1>
+              <h2 className="border-r-2 border-rtu-green"><b>Lekc. </b>Sistēmu analīze un zināšanu iegūšana</h2>
+              <h1 className="border-r-2 border-rtu-green">Āze. 12-102</h1>
+              <h1 className="border-r-2 border-rtu-green">Mārīte Kirikova</h1>
+              <h1 className="border-r-2 border-rtu-green"></h1>
+              <h1 className="border-r-2 border-rtu-green"></h1>
+              <h1 className="border-r-2 border-rtu-green"></h1>
+              <h1 className=""></h1>
           </div>
 
-          <div class="sm:flex mt-6">
-            <button onClick={() => setShowInfo(true)} class="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Mainīt norises informāciju</button>
+          <div className="sm:flex mt-6">
+            <button onClick={() => setShowInfo(true)} className="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Mainīt norises informāciju</button>
             {showInfo && createPortal(
               <ModalInfo onClose={() => setShowInfo(false)} />,
               document.body
               )}
-            <button onClick={() => setShowSubject(true)} class="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Mainīt tēmu</button>
+            <button onClick={() => setShowSubject(true)} className="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Mainīt tēmu</button>
             {showSubject && createPortal(
               <ModalSubject onClose={() => setShowSubject(false)} />,
               document.body
               )}
-            <button onClick={() => setShowGrade(true)} class="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Mainīt atzīmi</button>
+            <button onClick={() => setShowGrade(true)} className="bg-rtu-green hover:opacity-50 text-white py-2 px-4 rounded-lg mr-4">Mainīt atzīmi</button>
             {showGrade && createPortal(
               <ModalGrade onClose={() => setShowGrade(false)} />,
               document.body
